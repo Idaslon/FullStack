fed50a6f64ddda1cdd90313bf27dab3c6e1af48b
Idaslon <destroyeeru18@gmail.com>
2020-03-27 15:06:47 -0300

    User update fields added

--- gobarder/src/app/controllers/UserController.js
+++ gobarder/src/app/controllers/UserController.js
@@ -17,6 +17,36 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword) {
+      const isCorrectPassword = await user.isCorrectPassword(oldPassword);
+
+      if (!isCorrectPassword) {
+        return res.status(400).json({ error: 'Password did not match' });
+      }
+    }
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
 }
 
 export default new UserController();
