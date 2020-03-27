38c223cb5e375850e6053bb367475207a8553b45
DestroyeerU <destroyeeru18@gmail.com>
2020-03-27 15:43:03 -0300

    Input data validation

--- gobarder/src/app/controllers/UserController.js
+++ gobarder/src/app/controllers/UserController.js
@@ -1,7 +1,19 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    const schemaValid = await schema.isValid(req.body);
+    if (!schemaValid) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
@@ -19,6 +31,25 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    const schemaValid = await schema.isValid(req.body);
+    if (!schemaValid) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
     const { email, oldPassword } = req.body;
 
     const user = await User.findByPk(req.userId);
