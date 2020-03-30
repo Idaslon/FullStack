8c69a7a375664b24e7f5ec048d06ce81f40550a0
DestroyeerU <destroyeeru18@gmail.com>
2020-03-30 11:50:53 -0300

    Schedule bugs while merging fix

--- gobarder/src/routes.js
+++ gobarder/src/routes.js
@@ -8,6 +8,7 @@ import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
 import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -25,6 +26,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedules', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
