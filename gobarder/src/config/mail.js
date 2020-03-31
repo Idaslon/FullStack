a7b95d6b8097ae18719fecd12a7fe0e16de29170
DestroyeerU <destroyeeru18@gmail.com>
2020-03-31 00:01:22 -0300

    Mail sending started

---
+++ gobarder/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'f89406f5bc0093',
+    pass: 'd91b559ea09bb1',
+  },
+  default: {
+    from: 'Equipe Gobarder <noreply@gobarder.com>',
+  },
+};
