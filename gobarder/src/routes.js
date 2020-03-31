4695eaf61fcd01c2c8a6c3a5e80d376a3bd41bbd
DestroyeerU <destroyeeru18@gmail.com>
2020-03-31 01:59:36 -0300

    List available times for providers

--- gobarder/src/routes.js
+++ gobarder/src/routes.js
@@ -10,6 +10,7 @@ import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -24,6 +25,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:id/available', AvailableController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
