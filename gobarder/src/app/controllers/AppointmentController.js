bba88e808dab3b0805b22dc898633ccbf9922262
DestroyeerU <destroyeeru18@gmail.com>
2020-03-30 22:42:10 -0300

    MongoDB setup and notifications creating

--- gobarder/src/app/controllers/AppointmentController.js
+++ gobarder/src/app/controllers/AppointmentController.js
@@ -1,9 +1,12 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+
+import Notitication from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -82,6 +85,20 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date,
+    });
+
+    /*
+     * Notify appointment provider
+     */
+
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(startHour, "'dia' dd 'de' MMMM', às' Hmm'h'", {
+      locale: pt,
+    });
+
+    await Notitication.create({
+      content: `Novo agendamento de ${user.name} para o ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
--- gobarder/src/database/index.js
+++ gobarder/src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import databaseConfig from '../config/database';
 
@@ -11,6 +12,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -21,6 +23,17 @@ class Database {
       .map(
         (model) => model.associate && model.associate(this.connection.models)
       );
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarder',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      }
+    );
   }
 }
 
