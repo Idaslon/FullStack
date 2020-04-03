7b0d13d7f4e5b538e7831f8dec254beec913a467
DestroyeerU <destroyeeru18@gmail.com>
2020-04-02 22:21:40 -0300

    React initiaded

---
+++ react/modulo01/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './App.css'
+
+import logo from './assets/logo.png'
+
+function App() {
+  return (
+    <img width="400" src={logo} alt="Logo"/>
+  )
+}
+
+export default App;
