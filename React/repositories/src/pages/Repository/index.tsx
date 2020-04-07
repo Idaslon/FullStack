2b914aa56d37de076f97edc5a9e07c74910611fb
DestroyeerU <destroyeeru18@gmail.com>
2020-04-07 18:10:30 -0300

    List issues

--- React/repositories/src/pages/Repository/index.tsx
+++ React/repositories/src/pages/Repository/index.tsx
@@ -3,7 +3,7 @@ import { RouteComponentProps } from 'react-router-dom';
 
 import api from '@services/api';
 import Container from '@components/Container';
-import { Loading, Owner } from './styles';
+import { Loading, Owner, IssueList } from './styles';
 
 interface RouterProps {
   repository: string
@@ -11,15 +11,26 @@ interface RouterProps {
 
 type RepositoryProps = RouteComponentProps<RouterProps>
 
-interface RepositoryOwner {
+interface User {
   avatar_url: string
   login: string
 }
 
-interface Issue {}
+interface Label {
+  id: number
+  name: string
+}
+
+interface Issue {
+  id: number
+  user: User
+  labels: Label[]
+  html_url: string
+  title: string
+}
 
 interface RepositoryState {
-  owner: RepositoryOwner
+  owner: User
   issues: Issue[]
   name: string
   description: string
@@ -45,36 +56,42 @@ export default class Repository extends Component<RepositoryProps, RepositorySta
   }
 
   async componentDidMount() {
-    const { match } = this.props;
-    const repositoryName = decodeURIComponent(match.params.repository);
+    const state = localStorage.getItem('this');
+    if (state) {
+      this.setState(JSON.parse(state));
+    }
 
-    const [repository, issues] = await Promise.all([
-      api.get<RepositoryState>(`/repos/${repositoryName}`),
-      api.get<RouterProps>(`/repos/${repositoryName}/issues`, {
-        params: {
-          state: 'open',
-          // eslint-disable-next-line @typescript-eslint/camelcase
-          per_page: 5,
-        },
-      }),
-    ]);
+    // const { match } = this.props;
+    // const repositoryName = decodeURIComponent(match.params.repository);
 
-    const { avatar_url, login } = repository.data.owner;
+    // const [repository, issues] = await Promise.all([
+    //   api.get<RepositoryState>(`/repos/${repositoryName}`),
+    //   api.get<Issue[]>(`/repos/${repositoryName}/issues`, {
+    //     params: {
+    //       state: 'open',
+    //       // eslint-disable-next-line @typescript-eslint/camelcase
+    //       per_page: 5,
+    //     },
+    //   }),
+    // ]);
 
-    this.setState({
-      owner: {
-        avatar_url,
-        login,
-      },
-      description: repository.data.description,
-      name: repository.data.name,
-      loading: false,
-    });
+    // const { avatar_url, login } = repository.data.owner;
+
+    // this.setState({
+    //   owner: {
+    //     avatar_url,
+    //     login,
+    //   },
+    //   issues: issues.data,
+    //   description: repository.data.description,
+    //   name: repository.data.name,
+    //   loading: false,
+    // });
   }
 
   render() {
     const {
-      loading, owner, name, description,
+      loading, owner, name, description, issues,
     } = this.state;
 
     if (loading) {
@@ -90,6 +107,25 @@ export default class Repository extends Component<RepositoryProps, RepositorySta
           <h1>{name}</h1>
           <p>{description}</p>
         </Owner>
+        <IssueList>
+          {issues.map((issue) => (
+            <li key={issue.id}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map((label) => (
+                    <span key={label.id}>
+                      {label.name}
+                    </span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
       </Container>
     );
   }
--- React/repositories/src/pages/Repository/styles.ts
+++ React/repositories/src/pages/Repository/styles.ts
@@ -37,6 +37,63 @@ export const Owner = styled.div`
     max-width: 400px;
     color: #666;
   }
+`;
 
+export const IssueList = styled.ul`
+  border-top: 1px solid #eee;
+  margin-top: 10px;
+
+  img {
+    width: 35px;
+    height: 35px;
+    border-radius: 50%;
+  }
+
+  li + li {
+    margin-top: 15px;
+  }
+
+  li {
+    list-style: none;
+    border: 1px solid #eee;
+    border-radius: 5px;
+
+    margin-top: 25px;
+    padding: 10px;
+
+    display: flex;
+
+    div {
+      margin-left: 20px;
+      font-size: 15px;
+
+      a {
+        text-decoration: none;
+        color: #333;
+
+        &:hover {
+          color: #7f2ed0;
+        }
+      }
+
+      span {
+        margin-left: 5px;
+        padding: 2px;
+
+        font-size: 14px;
+        font-weight: 600;
+
+        background: #eee;
+        border-radius: 4px;
+      }
+
+      p {
+        margin-top: 7px;
+        color: #666;
+        font-size: 12px;
+      }
+    }
+
+  }
 
 `;
