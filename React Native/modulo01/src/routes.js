68782ebc2fe0a4dd9c029003f74b8113e463204b
DestroyeerU <destroyeeru18@gmail.com>
2020-04-11 03:22:32 -0300

    Navigation

---
+++ React Native/modulo01/src/routes.js
@@ -0,0 +1,26 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Main from './Main';
+import User from './User';
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main,
+      User,
+    },
+    {
+      defaultNavigationOptions: {
+        headerBackTitleVisible: false,
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#7f2ed0',
+        },
+        headerTintColor: '#FFF',
+      },
+    },
+  ),
+);
+
+export default Routes;
