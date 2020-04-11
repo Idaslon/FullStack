a80c081e5c0b5eba3c49bbd143451b1daf0c65f0
DestroyeerU <destroyeeru18@gmail.com>
2020-04-11 19:36:36 -0300

    Status bar added

--- React Native/modulo01/src/index.js
+++ React Native/modulo01/src/index.js
@@ -1,7 +1,15 @@
+import 'react-native-gesture-handler';
 import * as React from 'react';
+import { StatusBar } from 'react-native';
 
 import Routes from './routes';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7f2ed0" />
+      <Routes />
+      {/* <View /> */}
+    </>
+  );
 }
